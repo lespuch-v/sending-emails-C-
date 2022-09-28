98b589b0365e38b5adcd84787599007a36e3ccf5
Vasyl Lespuch <v.lespukh@gmail.com>
2022-09-28 13:41:12 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using MailKit.Net.Smtp;
+using MailKit;
+using MimeKit;
+
+namespace EmailSendProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MimeMessage message = new MimeMessage();
+            // Message from
+            message.From.Add(new MailboxAddress("Adam Duma", "testvl@seznam.cz"));
+
+            // Message to 
+            message.To.Add(MailboxAddress.Parse("example@gmail.com"));
+            message.Subject = "Hello World";
+            message.Body = new TextPart("plain")
+            {
+                Text = @"Yes, 
+                Hello!.
+                Buy Bitcoin!
+                "
+            };
+            // Authenticate here pass and login info to your account
+            string emailAddress = "";
+            string password = "";
+
+            SmtpClient client = new SmtpClient();
+            try
+            {
+                client.Connect("smtp.seznam.cz", 587, false);
+                client.Authenticate(emailAddress, password);
+                client.Send(message);
+                Console.WriteLine("Email Sent!");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                client.Disconnect(true);
+                client.Dispose();
+            }
+        }
+    }
+}
